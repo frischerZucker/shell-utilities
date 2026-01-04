853a678ed66e243f85182efc3340a10432ddd56b
Moritz Bärnt <moritzbaernt@outlook.de>
2026-01-04 14:44:17 +0100

    Add function to move stuff while creating folders if necessary.
    mv, but creates parent folders required by the target path if the do not exist.
    Works like "mkdir --parents" but for mv.

---
+++ mv-parents.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mv-parents () {
+# Move files and create folders required by the targets path if necessary.
+# (like 'mkdir --parents' but for mv)
+#
+# Arguments:
+#	1: source path
+#	2: target path
+
+    SOURCE_PATH=$1
+	TARGET_PATH=$2
+	
+	if [ ! "$TARGET_PATH" ]; then
+		echo "Too few arguments!"
+		echo "Use 'mv-parents [PATH TO SOURCE] [PATH TO TARGET]'."
+		exit 1
+	fi
+
+	if [ ! -d "$TARGET_DIR" ]; then
+		mkdir --parents "$(dirname "$TARGET_PATH")"
+	fi
+	
+	mv "$SOURCE_PATH" "$TARGET_PATH"
+}
